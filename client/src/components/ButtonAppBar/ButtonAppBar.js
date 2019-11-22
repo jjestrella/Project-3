348a40bf8402ce44134b46d8bc64112e0d595676
Kyle <kbrooks353@gmail.com>
2019-11-22 13:46:41 -0800

    moved the json packages around + added the navbar and a basic landing page

--- client/src/components/ButtonAppBar/ButtonAppBar.js
+++ client/src/components/ButtonAppBar/ButtonAppBar.js
@@ -4,8 +4,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+// import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -29,6 +29,7 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title}>
             BCS Stocks
           </Typography>
+          <Button color="inherit">Signup</Button>
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
---
+++ client/src/pages/LandingPage/landingpage.jsx
@@ -0,0 +1,27 @@
+import React, { Fragment } from "react";
+import User from '../../utils/Stores/User';
+import AppBar from "../../components/ButtonAppBar/ButtonAppBar"
+
+// function EmailMessage() {
+//     return (
+//         <Fragment>
+//             You must enter a valid email.
+//         </Fragment>
+//     );
+// }
+
+// function PasswordMessage() {
+//     return (
+//         <Fragment>
+//             You must enter a valid password.
+//         </Fragment>
+//     );
+// }
+
+export default function () {
+    User.refreshOnLoad();
+
+    return (
+       <AppBar />
+    );
+}
