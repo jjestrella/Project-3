e1f8625e99e8792c9789a9ed1cc74cd6bd7a809f
jjestrella <estrella.jjuan@gmail.com>
2019-12-02 11:04:56 -0800

    working on landingpage layout

--- client/src/pages/Home/landingpage.jsx
+++ client/src/pages/Home/landingpage.jsx
@@ -1,7 +1,11 @@
 // import React, { Fragment } from "react";
 import React from "react";
 import User from '../../utils/Stores/User';
-import AppBar from "../../components/ButtonAppBar/ButtonAppBar"
+import AppBar from "../../components/ButtonAppBar/ButtonAppBar";
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
 
 // function EmailMessage() {
 //     return (
@@ -18,11 +22,41 @@ import AppBar from "../../components/ButtonAppBar/ButtonAppBar"
 //         </Fragment>
 //     );
 // }
+const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }));
 
 export default function () {
     User.refreshOnLoad();
+    const classes = useStyles();
 
     return (
-       <AppBar />
-    );
-}
+       <AppBar />,
+       <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+      </Grid>
+    </div>
+       );
+    }
+
+
+
+
+
+
