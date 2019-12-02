4092e34b2f8a5a4425ac29b1cd9cca4fae991faa
Jason Osao <josako89@gmail.com>
2019-12-02 11:05:30 -0800

    adding market page

---
+++ client/src/pages/Market/market.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import User from "../../../utils/Stores/User"
+import API from "../../../utils/Stores/Cryptos/cryptoAPI"
+
+export default function(){
+    User.refreshOnLoad();
+
+    useEffect(() => {
+        API.getCryptos()
+            .then(res => {
+                console.log(res)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }, [])
+    
+    return(
+        <div>
+            this will be the member page
+        </div>
+    )
+}
