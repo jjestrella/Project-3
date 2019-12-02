4ee4da38adfc56f4d3431294a8f9dbad5b161f18
jjestrella <estrella.jjuan@gmail.com>
2019-12-02 13:41:11 -0800

    added placeholder pages on landing page and a sign up button in the body

--- client/src/pages/Home/landingpage.jsx
+++ client/src/pages/Home/landingpage.jsx
@@ -1,67 +1,74 @@
-<<<<<<< HEAD
-// import React, { Fragment } from "react";
-import React from "react";
+import React, {Fragment} from "react";
 import User from '../../utils/Stores/User';
-import AppBar from "../../components/ButtonAppBar/ButtonAppBar";
-import { makeStyles } from '@material-ui/core/styles';
+import AppBar from "../../components/AppBar/AppBar";
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
 
 
-// function EmailMessage() {
-//     return (
-//         <Fragment>
-//             You must enter a valid email.
-//         </Fragment>
-//     );
-// }
 
-// function PasswordMessage() {
-//     return (
-//         <Fragment>
-//             You must enter a valid password.
-//         </Fragment>
-//     );
-// }
 const useStyles = makeStyles(theme => ({
     root: {
-      flexGrow: 1,
-    },
-    paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    },
-  }));
-=======
-import React from "react";
-import User from '../../utils/Stores/User';
-import AppBar from "../../components/AppBar/AppBar"
+      padding: theme.spacing(25, 2),
+      backgroundColor: "#607d8b",
 
->>>>>>> origin
+    },
+    rootTwo: {
+        padding: theme.spacing(25, 2),
+      backgroundColor: "#fff8e1",
+    }, 
+    btnroot: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        
+      },
+      label: {
+        textTransform: 'capitalize',
+      },
+    
+  }));
+
 
 export default function () {
     User.refreshOnLoad();
     const classes = useStyles();
 
-    return (
-       <AppBar />,
-       <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
-        </Grid>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
-        </Grid>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
-        </Grid>
-      </Grid>
-    </div>
-       );
-    }
 
+    return (
+        <Fragment>
+       <AppBar />
+        <Paper className={classes.root}>
+        <Typography variant="h1" component="h3">
+          This is a sheet of paper.
+        </Typography>
+        <Typography component="p">
+          Paper can be used to build surface or other elements for your application.
+        </Typography>
+        <Button className={classes.btnroot} href="/signup" >Start Playing Today!</Button>
+      </Paper>
+      <Divider />
+
+      <Paper className={classes.rootTwo}>
+        <Typography variant="h5" component="h3">
+          This is a sheet of paper.
+        </Typography>
+        <Typography component="p">
+          Paper can be used to build surface or other elements for your application.
+        </Typography>
+      </Paper>
+    </Fragment>
+   );
+ }
+
+
+    
 
 
 
