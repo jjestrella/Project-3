eff36a9c707b6af841329aaf414f44f55961a3b2
Jason Osao <josako89@gmail.com>
2019-12-02 17:14:33 -0800

    modified market page to render coins as a list

--- client/src/pages/Market/market.jsx
+++ client/src/pages/Market/market.jsx
@@ -16,39 +16,36 @@ export default function(){
             })
     }, [])
 
-    // contructor(props) {
-    //     super(props);
-
-    //     const crytos = [];
-
-        for (let i = 0; i < res.data.data.length; i++) {
-            const data = createData(data.data.name, data.data.symbol, data.data.quote.USD.price, data.data.quote.USD.percent_change_24h, data.data.quote.USD.market_cap, data.data.circulating_supply, data.data.total_supply);
-            
-            res.data;
-           
-            rows.push(data);
-            }
-        }
     
-    
-    return(
-        <Fragment>
-            <AppBar/>
-                <Container>
-                    <Row>
-                        <Col>
-                        for (let i = 0; i < res.data.data.length; i++) {
-                            const data = createData(data.data.name, data.data.symbol, data.data.quote.USD.price, data.data.quote.USD.percent_change_24h, data.data.quote.USD.market_cap, data.data.circulating_supply, data.data.total_supply);
-            
-            res.data;
-           
-            rows.push(data);
-            }
-        }
-                        </Col>
-                    </Row>
-                    
-                </Container>
 
-        </Fragment>
-    )
+        // for (let i = 0; i < res.data.data.length; i++) {
+        //     const data = createData(data.data.name, data.data.symbol, data.data.quote.USD.price, data.data.quote.USD.percent_change_24h, data.data.quote.USD.market_cap, data.data.circulating_supply, data.data.total_supply);
+            
+        //     res.data;
+           
+        //     rows.push(data);
+        //     }
+        // }
+        constructor(props); {
+            super(props);
+        
+            const crypto = {};
+        
+            for (let i = 0; i < res.data.data.length;  i++) {
+                crypto = {
+                    name: data.data.name, symbol: data.data.symbol,currentPrice: data.data.quote.USD.price, percentChange: data.data.quote.USD.percent_change_24h, marketCap: data.data.quote.USD.market_cap, circulatingSupply: data.data.circulating_supply, totalSupply: data.data.total_supply
+                };
+            }
+        
+            this.state = { crypto };
+        }
+        
+        render(); {
+            return (
+                <Fragment>
+                <AppBar/>
+            
+        
+                </Fragment>
+    
+
